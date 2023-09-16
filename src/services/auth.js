93aaeb9b4dea2a2cc95086623fb31a7c66b4fc44
liem754 <pliem754@gmail.com>
2023-09-16 16:50:55 +0700

    first commit

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,8 +1,35 @@
 import db from "../models";
-export const register = () =>
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const hashPassword = (pw) => bcrypt.hashSync(pw, bcrypt.genSaltSync(8));
+export const register = ({ name, email, password }) =>
   new Promise(async (resolve, reject) => {
     try {
-      resolve("thanh cong");
+      const response = await db.User.findOrCreate({
+        where: { email },
+        defaults: {
+          email,
+          password: hashPassword(password),
+          name,
+        },
+      });
+      const token = response[1]
+        ? jwt.sign(
+            {
+              id: response[0].id,
+              email: response[0].email,
+              role: response[0].role,
+            },
+            process.env.JWT_KEY,
+            { expiresIn: "2d" }
+          )
+        : null;
+
+      resolve({
+        err: response[1] ? 0 : 1,
+        mes: response[1] ? "Register is success!" : "Email has used!",
+        token,
+      });
     } catch (error) {
       reject(error);
     }
