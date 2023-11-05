055e2c22761f69ee167ab776a255a7d5d343723b
liem754 <pliem754@gmail.com>
2023-11-05 14:33:19 +0700

    dfkf

--- src/middlewares/verify_token.js
+++ src/middlewares/verify_token.js
@@ -3,7 +3,9 @@ import { handleNotAuth } from "./handle_error";
 const verifyToken = (req, res, next) => {
   const token = req.headers.authorization;
   if (!token) return handleNotAuth("Require authorization", res);
+
   const accesstoken = token.split(" ")[1];
+  console.log(accesstoken);
   jwt.verify(accesstoken, process.env.JWT_KEY, (err, user) => {
     if (err) return handleNotAuth("Access token expired", res);
     console.log(user);
